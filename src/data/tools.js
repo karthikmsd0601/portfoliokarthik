dee30e5440e9ea1e3c39507fc39927f06a45a141
Avin <avin@vaanam.com>
2026-08-04 12:12:37 +0530

    remoev capcut

--- src/data/tools.js
+++ src/data/tools.js
@@ -1,11 +1,11 @@
 import { IllustratorLogo } from '../assets/icons/ToolLogos'
-import capcutImg from '../assets/images/Tools/capcut.png'
+// import capcutImg from '../assets/images/Tools/capcut.png'
 import figmaImg from '../assets/images/Tools/figma.png'
 import canvaImg from '../assets/images/Tools/canva.png'
 import photoshopImg from '../assets/images/Tools/ps.png'
 
 export const tools = [
-  { id: 'capcut', name: 'CapCut', image: capcutImg },
+  // { id: 'capcut', name: 'CapCut', image: capcutImg },
   { id: 'figma', name: 'Figma', image: figmaImg },
   { id: 'canva', name: 'Canva', image: canvaImg },
   { id: 'photoshop', name: 'Photoshop', image: photoshopImg },
